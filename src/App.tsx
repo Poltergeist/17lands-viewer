857231c2cc37745fdec7fb96e586be12df1ea29f
Poltergeist <spectator.networx@gmail.com>
2023-09-25 11:38:19 +0200

    feat: add various features
    Dark mode
    Image grid
    first steps toward filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import Papa from "papaparse";
+import {
+  Box,
+  Button,
+  Container,
+  Image,
+  ListItem,
+  SimpleGrid,
+  Textarea,
+  UnorderedList,
+  Select,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 type CsvData = {
   Name: string;
   "GP WR": string;
+  Rarity: "C" | "U" | "R" | "M";
+  Color: string;
 };
 
 const parsePercent = (percantage: string) => {
@@ -12,7 +26,7 @@ const parsePercent = (percantage: string) => {
 };
 function App() {
   const [data, setData] = useState<CsvData[]>([]);
-  const [cardData, setCardData] = useState({});
+  const [cardData, setCardData] = useState<{ [key: string]: string }>({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -64,8 +78,8 @@ function App() {
   if (data.length === 0) {
     return (
       <>
-        <div>
-          <textarea
+        <Container>
+          <Textarea
             onChange={(event) => {
               setData(
                 Papa.parse<CsvData>(event.target.value, {
@@ -75,30 +89,59 @@ function App() {
               setLoading(true);
             }}
           />
-        </div>
+        </Container>
       </>
     );
   }
   return (
     <>
-      <div>
-        <button onClick={() => setData([])}>reset</button>
-        {data
-          .sort((a, b) =>
-            parsePercent(a["GP WR"]) === parsePercent(b["GP WR"])
-              ? 0
-              : parsePercent(a["GP WR"]) > parsePercent(b["GP WR"])
-              ? -1
-              : 1
-          )
-          .map((card) => (
-            <img
-              key={card.Name}
-              src={cardData[card.Name]}
-              title={`${card.Name} - ${card["GP WR"]}`}
-            />
-          ))}
-      </div>
+      <Container maxW="container.lg">
+        <Box>
+          <SimpleGrid columns={3}>
+            <FormControl>
+              <FormLabel>Filter for Rarity</FormLabel>
+              <Select placeholder="Select Rarity">
+                <option value="C">Common</option>
+                <option value="U">Uncommon</option>
+                <option value="R">Rare</option>
+                <option value="M">Mythic</option>
+              </Select>
+            </FormControl>
+            <Box>
+              <Button onClick={() => setData([])}>reset</Button>
+            </Box>
+          </SimpleGrid>
+        </Box>
+        <SimpleGrid columns={4}>
+          {data
+            .sort((a, b) =>
+              parsePercent(a["GP WR"]) === parsePercent(b["GP WR"])
+                ? 0
+                : parsePercent(a["GP WR"]) > parsePercent(b["GP WR"])
+                ? -1
+                : 1
+            )
+            .map((card) => (
+              <Box
+                key={card.Name}
+                maxW="sm"
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+              >
+                <Image
+                  src={cardData[card.Name]}
+                  title={`${card.Name} - ${card["GP WR"]}`}
+                />
+                <Box p="3">
+                  <UnorderedList>
+                    <ListItem> Win Rate: {card["GP WR"]}</ListItem>
+                  </UnorderedList>
+                </Box>
+              </Box>
+            ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import theme from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
